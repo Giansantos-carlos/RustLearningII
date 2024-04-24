9b6a99c5d47070097946f6caa5498412f3df9ef9
GianSantos <giansantos.carlos@gmail.com>
2024-04-23 21:51:12 -0300

    convert to int

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use std::io;
+
+fn convert_to_integer(data: & String) ->i32 {
+    let data: i32 = data.trim().parse::<i32>().unwrap();
+    data
+}
+
 fn main() {
+    /* 
     let mut name = "Gain"; // mut is mutable variable
     name = "Olga";
     println!("name: {}", name);
+    */
+
+    let mut numberOne = String::new();
+    io::stdin().read_line(&mut numberOne).expect("Error reading input numberOne");
+
+    let mut numberTwo = String::new();
+    io::stdin().read_line(&mut numberTwo).expect("Error reading input numberTwo");
+
+    if convert_to_integer(&numberOne) > convert_to_integer(&numberTwo) {
+        print!("numberOne {} is must of numberTwo {}", numberOne, numberTwo);
+    } else {
+        print!("numberTwo {} is must of numberOne {}", numberTwo, numberOne);
+    }
+
 }
+
+
+// types datas
+
+//let x: I32 =  9; Integer
+//let y: u32 = 10; not negative 
+// let z: f32 = 11.4; floating point
+// let w: bool = false;
